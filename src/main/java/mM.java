f9a6d61339b3390e7029517915364a51bf9ecfb3
Nader <ahmednader839@gmail.com>
2015-11-04 22:41:32 +0200

    gitignore and removed comments

--- src/main/java/mM.java
+++ src/main/java/mM.java
@@ -10,10 +10,6 @@ public class mM {
             String str;
 
             br = new BufferedReader(new InputStreamReader(getClass().getResourceAsStream("/KLSadd.tex")));
-
-          /*  File file = new File(getClass().getResource("output.tex").toURI());
-            file.createNewFile();
-            PrintWriter writer = new PrintWriter(file);*/
             PrintStream out = new PrintStream(new FileOutputStream("output.tex"));
             System.setOut(out);
             while ((str = br.readLine()) != null) {
@@ -30,12 +26,9 @@ public class mM {
                     }
                 }
                 for(int j = 0; j<result.length; j++) {
-                    //writer.print(result[j]);
                     System.out.print(result[j]);
                 }
                 System.out.println();
-              //  writer.println();
-                //writer.close();
             }
 
         } catch (IOException e) {
@@ -50,28 +43,6 @@ public class mM {
     }
 
     public static void main(String[]args) throws NumberFormatException, IOException, URISyntaxException {
-
-        /*BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        System.out.println("Input a string");
-        String str = br.readLine();
-        char [] result = new char[str.length()];
-
-        for ( int i = 0; i < str.length(); i++)
-        {
-            if(str.charAt(i) == 'm') {
-                result[i] = 'M';
-            }
-            else if(str.charAt(i) == 'M'){
-                    result[i] = 'm';
-                } else{
-                result[i] = str.charAt(i);
-            }
-        }
-        for(int j = 0; j<result.length; j++) {
-            System.out.print(result[j]);
-        }
-        */
-
         mM x= new mM();
         x.change();
 
