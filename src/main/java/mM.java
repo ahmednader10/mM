d6163ba188d698756dc25b32b8952ff5e6127d2b
Nader <ahmednader839@gmail.com>
2015-11-05 15:49:30 +0200

    added test case and removed maven.iml and modified catching exceptions

--- src/main/java/mM.java
+++ src/main/java/mM.java
@@ -2,42 +2,48 @@ import java.io.*;
 import java.net.URISyntaxException;
 
 public class mM {
-    public void change() throws URISyntaxException {
-        try {
-
-            String str;
-
-            BufferedReader br = new BufferedReader(new InputStreamReader(getClass().getResourceAsStream("/KLSadd.tex")));
+    public void change(String x) throws URISyntaxException, IOException {
+        String str;
+        BufferedReader br = null;
+        if (x == null) {
+            br = new BufferedReader(new InputStreamReader(getClass().getResourceAsStream("/KLSadd.tex")));
             PrintStream out = new PrintStream(new FileOutputStream("output.tex"));
             System.setOut(out);
             while ((str = br.readLine()) != null) {
-                char [] result = new char[str.length()];
-                for ( int i = 0; i < str.length(); i++)
-                {
-                    if(str.charAt(i) == 'm') {
-                        result[i] = 'M';
-                    }
-                    else if(str.charAt(i) == 'M'){
-                        result[i] = 'm';
-                    } else{
-                        result[i] = str.charAt(i);
-                    }
-                }
-                for(int j = 0; j<result.length; j++) {
-                    System.out.print(result[j]);
-                }
+                mM(str);
                 System.out.println();
             }
-
-        } catch (IOException e) {
-            e.printStackTrace();
+        } else {
+            mM(x);
         }
     }
+    public char[] mM(String str) {
+        char [] result = new char[str.length()];
+        for ( int i = 0; i < str.length(); i++)
+        {
+            if(str.charAt(i) == 'm') {
+                result[i] = 'M';
+            }
+            else if(str.charAt(i) == 'M'){
+                result[i] = 'm';
+            } else{
+                result[i] = str.charAt(i);
+            }
+        }
+        for(int j = 0; j<result.length; j++) {
+            System.out.print(result[j]);
+        }
+        return result;
+    }
 
-    public static void main(String[]args) throws NumberFormatException, IOException, URISyntaxException {
+    public String charToString(char[] x) {
+        String out = String.valueOf(x);
+        return out;
+    }
+
+    public static void main(String[]args) throws  IOException, URISyntaxException {
         mM x= new mM();
-        x.change();
-
+        x.change(null);
 
     }
 }
--- src/test/java/mMTest.java
+++ src/test/java/mMTest.java
@@ -7,6 +7,7 @@ public class mMTest extends TestCase {
 
     @org.junit.Test(timeout = 1000)
     public void testName() throws Exception {
-
+        mM test = new mM();
+        assertEquals("my naMe is AhMed", test.charToString(test.mM("My name is Ahmed")));
     }
 }
